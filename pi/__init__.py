e7eb12f25d24ba927dbae48ede99f0eb5ac03e29
PythonFZ <fabian.zills@web.de>
2023-02-02 14:44:44 +0100

    add BaileyBorweinPlouffeEstimator

--- pi/__init__.py
+++ pi/__init__.py
@@ -30,3 +30,30 @@ class LeibnizPiEstimator(PiEstimator):
         data = np.arange(self.n)
         pi = 4 * (-1) ** data / (2 * data + 1)
         return float(np.sum(pi))
+
+
+class BaileyBorweinPlouffeEstimator(PiEstimator):
+    """Estimate pi using the Bailey-Borwein-Plouffe formula."""
+
+    def __init__(self, n):
+        """Initialize the Bailey-Borwein-Plouffe formula estimator.
+
+        Parameters
+        ----------
+        n: int
+            Number of points to use.
+        """
+        self.n = n
+
+    def estimate(self):
+        """Estimate pi using the Bailey-Borwein-Plouffe formula."""
+        data = np.arange(self.n)
+        pre_factor = 1 / (16**data)
+        pi = (
+            4 / (8 * data + 1)
+            - 2 / (8 * data + 4)
+            - 1 / (8 * data + 5)
+            - 1 / (8 * data + 6)
+        )
+        pi = pre_factor * pi
+        return float(np.sum(pi))
--- test_pi.py
+++ test_pi.py
@@ -15,3 +15,9 @@ def test_PiEstimator():
 def test_LeibnizPiEstimator(n):
     """Test the Leibniz PiEstimator."""
     assert pi.LeibnizPiEstimator(n).estimate() == pytest.approx(np.pi, 0.01)
+
+
+@pytest.mark.parametrize("n", [2, 3, 4, 5, 6, 7, 8])
+def test_BaileyBorweinPlouffe(n):
+    """Test the BaileyBorweinPlouffe Estimator."""
+    assert pi.BaileyBorweinPlouffeEstimator(n).estimate() == pytest.approx(np.pi, 0.001)
